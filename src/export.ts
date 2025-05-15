a123253c93488f4965d1ef44bba06c14450485cb
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-05-15 12:38:58 -0300

    feat: melhorando visualização do script

--- src/export.ts
+++ src/export.ts
@@ -4,44 +4,43 @@ import { createWriteStream } from 'node:fs';
 import { stringify } from 'csv-stringify';
 import { sql } from './database/client.js';
 
-const query = sql`
-  SELECT id, name
-  FROM products
-  WHERE price_in_cents >= 1000
-`;
+console.time('Tempo de exportação');
 
+const query = sql`SELECT * FROM stock_terminal`;
 const cursor = query.cursor(500);
 
-// for await (const rows of cursor) {
-//   console.log(rows);
-//   break;
-// }
+let isFirstChunk = true;
+let dynamicStringifier;
 
-const exampleStream = new Transform({
+const captureColumnsAndTransform = new Transform({
   objectMode: true,
   transform(chunk, encoding, callback) {
-    // console.log(chunk);
+    if (isFirstChunk) {
+      const firstRow = chunk[0];
+      const columns = Object.keys(firstRow);
+
+      dynamicStringifier = stringify({
+        header: true,
+        columns,
+      });
+
+      this.pipe(dynamicStringifier).pipe(createWriteStream('./export.csv', 'utf-8'));
+      isFirstChunk = false;
+    }
+
     for (const item of chunk) {
-      // this.push(JSON.stringify(item).concat('\n')); // para exportar no formato jsonl
       this.push(item);
     }
+
     callback();
-  },
+  }
 });
 
 await pipeline(
   cursor,
-  exampleStream,
-  // createWriteStream('./export.jsonl', 'utf-8') // para exportar no formato jsonl
-  stringify({
-    delimiter: ',',
-    header: true,
-    columns: [
-      { key: 'id', header: 'ID' },
-      { key: 'name', header: 'Nome' },
-    ],
-  }),
-  createWriteStream('./export.csv', 'utf-8')
+  captureColumnsAndTransform
 );
 
 await sql.end();
+
+console.timeEnd('Tempo de exportação');
